c993b4b2b086f32af05697ab2466961274d23912
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-08-21 21:28:35 +0900

    feat: create component related modules

--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
 import {ConfigModule} from "@nestjs/config"
+import { LikeModule } from './components/like/like.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), LikeModule],
   controllers: [NestarBatchController],
   providers: [NestarBatchService],
 })
