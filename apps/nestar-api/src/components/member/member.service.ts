03cb5a675f736cb5f3776004f01f5ed8e90b60fe
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-08-31 01:18:07 +0900

    fix: modify view modele to record visits

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -8,10 +8,15 @@ import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { T } from '../../libs/types/common';
+import { ViewService } from '../view/view.service';
+import { ViewGroup } from '../../libs/enums/view.enum';
+import { ViewInput } from '../../libs/dto/view/view.input';
 
 @Injectable()
 export class MemberService {
-    constructor(@InjectModel('Member') private readonly memberModel: Model<Member>, private authService: AuthService) { }
+    constructor(@InjectModel('Member') private readonly memberModel: Model<Member>, 
+    private authService: AuthService,
+    private viewService: ViewService) { }
 
     public async signup(input: MemberInput):Promise<Member>{
         input.memberPassword = await this.authService.hashPassword(input.memberPassword);
@@ -64,15 +69,29 @@ export class MemberService {
 
     }
 
-    public async getMember(targetId: ObjectId):Promise<Member>{
+    public async getMember(memberId: ObjectId, targetId: ObjectId):Promise<Member>{
         const search:T ={
             _id:targetId,
             memberStatus:{
                $in:[MemberStatus.ACTIVE,MemberStatus.DELETE]
             } 
         };
-        const targetMember = await this.memberModel.findOne(search);
+        const targetMember = await this.memberModel.findOne(search).lean().exec();
         if(!targetMember) throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+        
+        if(memberId){
+            const viewInput = {
+                memberId:memberId,
+                viewRefId:targetId,
+                viewGroup:ViewGroup.MEMBER
+            }
+            const newView = await this.viewService.recordView(viewInput);
+            if(newView){
+                await this.memberModel.findByIdAndUpdate(search, {$inc:{ memberViews: 1}}, {new:true}).exec();
+                targetMember.memberViews++;
+            }
+
+        };
         
         return targetMember;
     }
