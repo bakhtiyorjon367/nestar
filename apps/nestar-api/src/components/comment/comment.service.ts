f778ae44db31f30e5f3459837304fcb6a25dbaae
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-09-10 00:27:25 +0900

    feat: configure comment module & create comment related resolver and service model

---
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CommentModule } from './comment.module';
+
+@Injectable()
+export class CommentService {
+    }
