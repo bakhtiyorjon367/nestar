63bb37da2fa3d90fe050f60f328d46b9725a2a51
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-09-11 22:31:32 +0900

    feat: create likeTargetMember GraphQL API

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -2,23 +2,10 @@ import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import LikeSchema from '../../schemas/Like.model';
-import { MemberModule } from '../member/member.module';
-import { AuthModule } from '../auth/auth.module';
-import { PropertyModule } from '../property/property.module';
-import { BoardArticle } from '../../libs/dto/board-article/board-article';
-import { BoardArticleModule } from '../board-article/board-article.module';
 
 @Module({
   imports:[
-    MongooseModule.forFeature([{
-      name:'Like',
-      schema: LikeSchema,
-    },
-  ]),
-  AuthModule,
-  MemberModule,
-  PropertyModule,
-  BoardArticleModule,
+    MongooseModule.forFeature([{name:'Like',schema: LikeSchema}]),
   ],
   providers: [LikeService],
   exports:[LikeService]
