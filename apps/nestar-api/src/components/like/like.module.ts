5a4aa57e2c62dd8f566578e10b9b123f8f39dc2f
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-09-11 21:54:14 +0900

    feat: crate likeService model

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { LikeService } from './like.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import LikeSchema from '../../schemas/Like.model';
+import { MemberModule } from '../member/member.module';
+import { AuthModule } from '../auth/auth.module';
+import { PropertyModule } from '../property/property.module';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleModule } from '../board-article/board-article.module';
 
-@Module({})
+@Module({
+  imports:[
+    MongooseModule.forFeature([{
+      name:'Like',
+      schema: LikeSchema,
+    },
+  ]),
+  AuthModule,
+  MemberModule,
+  PropertyModule,
+  BoardArticleModule,
+  ],
+  providers: [LikeService],
+  exports:[LikeService]
+})
 export class LikeModule {}
