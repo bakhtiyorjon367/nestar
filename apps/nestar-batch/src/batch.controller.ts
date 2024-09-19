9505c366a76f7ab1232d001b2b36371ef304b9e9
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-09-19 19:37:44 +0900

    fix: modify jobScheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -8,17 +8,12 @@ export class BatchController {
   constructor(private readonly batchService: BatchService) {}
   private logger:Logger = new Logger('SocketEventsGateway');
 
-  // @Interval(5000)
-  // hadnleInterval(){
-  //   this.logger.debug("interval Test");
-  // }
-
   @Timeout(1000)
   hadnleTimeout(){
-    this.logger.debug("Timeout test");
+    this.logger.debug("Batch Server Ready");
   }
 
-  @Cron("00 * * * * *", {name:BATCH_ROLLBACK})
+  @Cron("00 00 01 * * *", {name:BATCH_ROLLBACK})
   public async batchRollback(){
     try{
       this.logger["context"] = BATCH_ROLLBACK;
@@ -31,34 +26,39 @@ export class BatchController {
     }
   }//_______________________________________________________________
 
-  @Cron("20 * * * * *", {name:BATCH_TOP_PROPERTIES})
-  public async batchProperties(){
+  @Cron("20 00 01 * * *", {name:BATCH_TOP_PROPERTIES})
+  public async batchTopProperties(){
     try{
       this.logger["context"] = BATCH_TOP_PROPERTIES;
       this.logger.debug("EXECUTED");
   
-      await this.batchService.batchProperties();
+      await this.batchService.batchTopProperties();
     }catch(err){
       this.logger.error(err);
     }
   }//_______________________________________________________________
 
-  @Cron("40 * * * * *", {name:BATCH_TOP_AGENTS})
-  public async batchAgents(){
+  @Cron("40 00 01 * * *", {name:BATCH_TOP_AGENTS})
+  public async batchTopAgents(){
     try{
       this.logger["context"] = BATCH_TOP_AGENTS;
       this.logger.debug("EXECUTED");
   
-      await this.batchService.batchAgents();
+      await this.batchService.batchTopAgents();
     }catch(err){
       this.logger.error(err);
     }
   }//_______________________________________________________________
 
+ // @Interval(5000)
+  // hadnleInterval(){
+  //   this.logger.debug("interval Test");
+  // }
 
   @Get()
   getHello(): string {
     return this.batchService.getHello();
-  }
+  }//_______________________________________________________________
+
 }
 
